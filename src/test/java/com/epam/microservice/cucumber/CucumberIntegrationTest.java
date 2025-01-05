0e86188c3f3dd2b2d26af574d74244c8e5782e94
Alex20081234 <raskosov.alex.jr@gmail.com>
2025-01-05 13:21:50 +0100

    Small change to property.

--- src/test/java/com/epam/microservice/cucumber/CucumberIntegrationTest.java
+++ src/test/java/com/epam/microservice/cucumber/CucumberIntegrationTest.java
@@ -9,7 +9,7 @@ import org.junit.platform.suite.api.Suite;
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("com/epam/microservice/steps")
-@ConfigurationParameter(key = Constants.FEATURES_PROPERTY_NAME, value = "src/test/resources/features/workload.feature")
+@ConfigurationParameter(key = Constants.FEATURES_PROPERTY_NAME, value = "src/test/resources/features")
 @ConfigurationParameter(key = Constants.GLUE_PROPERTY_NAME, value = "com/epam/microservice/steps")
 @ConfigurationParameter(key = Constants.PLUGIN_PROPERTY_NAME, value = "pretty, html:target/cucumber-report/cucumber.html")
 public class CucumberIntegrationTest {}
